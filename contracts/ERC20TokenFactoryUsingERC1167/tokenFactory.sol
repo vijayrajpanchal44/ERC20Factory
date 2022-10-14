244dea7c39f77f7099e58ad6153e1ab08da11a67
Vijay Raj Panchal <vijayrajpanchal4@gmail.com>
2022-10-14 18:55:32 +0530

    maintain clone contracts

--- contracts/ERC20TokenFactoryUsingERC1167/tokenFactory.sol
+++ contracts/ERC20TokenFactoryUsingERC1167/tokenFactory.sol
@@ -1,11 +1,25 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
-// import "@openzeppelin/contracts-upgradeable/presets/ERC20PresetFixedSupplyUpgradeable.sol";
-// import "@openzeppelin/contracts/proxy/Clones.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 import "./ERC20Token.sol";
+
 
 contract tokenFactory{
     address[] public createdTokens;
+
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIdCounter;
+
+    struct cloneTokenDetail{
+        uint256 tokenId;
+        string tokenName;
+        string tokenSymbol;
+        address tokenAddress;
+        address referenceTokenAddress;
+    }
+
+    mapping (uint256 => cloneTokenDetail) public idToCloneTokenDetail;
 
     event TokenCreated(
         address referenceToken,
@@ -17,9 +31,12 @@ contract tokenFactory{
 
     function create(string memory name, string memory symbol,uint8 decimal, address template)
     external returns (address) {
+        _tokenIdCounter.increment();
+        uint256 tokenId = _tokenIdCounter.current();
         address wallet = createClone(template);
         ERC20Token(wallet).initialize(name, symbol, decimal);
         createdTokens.push(wallet);
+        idToCloneTokenDetail[tokenId] = cloneTokenDetail(tokenId, name,symbol,wallet, template);
         emit TokenCreated(template, wallet, name, symbol, decimal);
         return wallet;
     }
